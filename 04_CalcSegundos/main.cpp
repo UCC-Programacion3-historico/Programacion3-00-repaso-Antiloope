061f65fe32bc39a070514082bfd78d0d3536c6dc
Antiloope <rodrigopizarro1234@gmail.com>
2017-08-10 19:10:38 -0300

    Se realizo el ejercicio 4 de Conversion de segundos completo - Guia 00 - Prg 3

--- 04_CalcSegundos/main.cpp
+++ 04_CalcSegundos/main.cpp
@@ -3,6 +3,18 @@
 using namespace std;
 
 int main() {
-    cout << "Ejercicio 00/04\n" << endl;
+    int a;
+    int h=0,m=0,s=0;
+    printf("Ingrese el valor en segundos: ");
+    scanf("%d",a);
+    
+    s = a%60;
+    m = a/60;
+    h = m/60;
+    if(m>60) {
+        m = m%60;
+    }
+
+    printf("%d segundos son: %d horas, %d minutos , %d segundos",a,h,m,s);
     return 0;
 }
