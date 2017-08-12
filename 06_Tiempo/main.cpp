a0e12657376ff1b5ee73f581d888eff35ab06124
Antiloope <rodrigopizarro1234@gmail.com>
2017-08-12 17:19:46 -0300

    Ejercicio 6 Tiempo Terminado - Guia 00 - Prg 3

--- 06_Tiempo/main.cpp
+++ 06_Tiempo/main.cpp
@@ -6,12 +6,14 @@ using namespace std;
 int main() {
     clock_t begin;
     begin = clock();
-
-    cout << "Comenzando a medir Tiempo\n" << endl;
+    int a[1024];
+    for (size_t i = 0; i < 1024; i++) {
+        a[i]=1000;
+    }
     clock_t end = clock();
 
     double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
 
-    cout << "Tardo elapsed_secs" << elapsed_secs << "\n" << std::endl;
+    cout << "Tardo elapsed_secs" << elapsed_secs << "\n" << endl;
     return 0;
 }
